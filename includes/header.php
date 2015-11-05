6257cf1e9f0fc4244981bd4902589353625f3ac8
Bastian Kopf <bastian.kopf@outlook.de>
2015-11-05 17:07:54 +0100

    add placeholder logo and menu icon

--- includes/header.php
+++ includes/header.php
@@ -7,5 +7,6 @@
 	<link href="css/style.css" type="text/css" rel="stylesheet" />
 	<script src="libs/jquery-2.1.4.js"></script>
 	<link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>The Facts WebApp</title>
   </head>
