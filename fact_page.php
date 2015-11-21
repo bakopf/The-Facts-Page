dbf7e5654c2010240e2b1c3645d919d9a7b36c0c
Bastian Kopf <bastian.kopf@outlook.de>
2015-11-21 14:25:00 +0100

    had to change script placement

--- fact_page.php
+++ fact_page.php
@@ -2,25 +2,22 @@
 include('includes/header.php');
 ?>
 <script>
-$(document).ready(function(){
-	$(".reload").click(function(){
-		$.ajax({
-		  type: "POST",
-		  url: "database/read_fact.php",
-		  datatype: "html",
-		  success: function(data) {
-		    $("#fact").html(data);
-		    }
-		});
+
+function reloadFact(){
+	$.ajax({
+	  type: "POST",
+	  url: "database/read_fact.php",
+	  datatype: "html",
+	  success: function(data) {
+	    $("#fact").html(data);
+	    }
 	});
-});
-
-
+}
 
 </script>
 
 	<div data-role="content">
-    	<a class="reload" data-role="button">get f4ct</a> 
+    	<a onclick="reloadFact();" data-role="button">get f4ct</a> 
 
     	<p id="fact"></p>
     </div>
--- write_page.php
+++ write_page.php
@@ -1,8 +1,46 @@
 <?php
 	include('./includes/header.php');
 ?>
+<script>
+	$(document).ready(function(){
+		$('.formsuccess').css("display","none");
+		$('.ausfuellen').css("display","none");
 
+		$("#sendfact").click(function(){
 
+			var fact = $("#fact").val();
+
+			$.ajax({
+				url: 'database/write_fact.php',
+				type:'POST',
+				data:
+				{
+					'fact': fact,
+				},
+				success: function(response)
+				{
+					if(response == "nein"){
+						$('.ausfuellen').css("display","block");
+					}else if (response == "fehler"){
+						$("#error").show();
+					} else{
+						$('.factform').css("display","none");
+						$('.formsuccess').css("display","block");
+						$('.ausfuellen').css("display","none");
+
+					}
+					
+				},
+				error: function(data){
+					alert(data.error.msg);
+					$("#error").show();
+				}
+				
+			});
+				
+		});
+	});
+</script>
 
 <div data-role="content">
 <h1>Send a Fact</h1>
